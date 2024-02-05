58ca53d1a6b7c4657df437db095ca4f383650c2e
NikitinSemen <pr.pypblrka@gmail.com>
2024-02-05 20:17:52 +0400

    разложил по директориям файлы. добавил tests

--- src/main.py
+++ src/main.py
@@ -1,22 +1,17 @@
-from utils import get_data_json
-from utils import get_new_operation
-from utils import get_sort_operation
-from utils import get_five_operation
-from utils import get_format_date
-from utils import vanish_from
-from utils import vanish
-import datetime
+from utils.utils import get_data_json
+from utils.utils import get_new_operation
+from utils.utils import get_sort_operation
+from utils.utils import get_five_operation
+from utils.utils import get_format_date
+from utils.utils import vanish_from
+from utils.utils import vanish
+
 
 operation = get_data_json("operations.json")
 new_operation = get_new_operation(operation)
 sort_operation = get_sort_operation(new_operation)
 five_last_operation = get_five_operation(sort_operation)
 
-
-
-
 for i in five_last_operation:
-
     print(get_format_date(i['date']), i['description'], '\n', vanish_from(i.get('from', 'Новый счет')),
           "->", vanish(i["to"]), "\n", i["operationAmount"]["amount"], '\n\n')
-
