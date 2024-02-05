7ce34f94e08eb4cd9fa7495d52665d70146dc81d
NikitinSemen <pr.pypblrka@gmail.com>
2024-02-05 19:52:13 +0400

    Написаны функции, написан код. uttils.py main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import json
+from utils import get_data_json
+from utils import get_new_operation
+from utils import get_sort_operation
+from utils import get_five_operation
+from utils import get_format_date
+from utils import vanish_from
+from utils import vanish
 import datetime
 
-file_name = "operations.json"
-with open(file_name) as file:
-    operation = json.load(file)
+operation = get_data_json("operations.json")
+new_operation = get_new_operation(operation)
+sort_operation = get_sort_operation(new_operation)
+five_last_operation = get_five_operation(sort_operation)
 
-    for i in operation:
-        print(i['date'].)
+
+
+
+for i in five_last_operation:
+
+    print(get_format_date(i['date']), i['description'], '\n', vanish_from(i.get('from', 'Новый счет')),
+          "->", vanish(i["to"]), "\n", i["operationAmount"]["amount"], '\n\n')
+
---
+++ utils.py
@@ -0,0 +1,41 @@
+import json
+import datetime
+
+
+def get_data_json(file_name):
+    with open(file_name) as file:
+        operation = json.load(file)
+        return operation
+
+
+def get_new_operation(operation):
+    oper_dict = []
+    for i in operation:
+        if i.get('state') == 'EXECUTED' and i.get('date'):
+            oper_dict.append(i)
+    return oper_dict
+
+
+def get_sort_operation(operation_list):
+    sorted_operation = sorted(operation_list, key=lambda operation: operation.get('date'), reverse=True)
+
+    return sorted_operation
+
+
+def get_five_operation(sorted_operation):
+    return sorted_operation[0:5]
+
+
+def get_format_date(date):
+    split_date = date.split('T')[0]
+    date_format = datetime.datetime.strptime(split_date, "%Y-%m-%d")
+    new_date_time = date_format.strftime("%d-%m-%Y")
+    return new_date_time
+
+
+def vanish(number):
+    return "**" + number[-4:]
+
+
+def vanish_from(number):
+    return number[0:17] + "**** "
