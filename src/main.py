9e42fea1d1dd9694f34894df06da9a5019f8230c
NikitinSemen <pr.pypblrka@gmail.com>
2024-02-06 21:23:24 +0400

    изменен импорт, файл загрузки выведен в константу, выровнен принт

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
-from utils.utils import get_data_json
-from utils.utils import get_new_operation
-from utils.utils import get_sort_operation
-from utils.utils import get_five_operation
-from utils.utils import get_format_date
-from utils.utils import mask_numb
-from utils.utils import mask_number
+from utils.utils import (
+    get_data_json,
+    get_new_operation,
+    get_format_date,
+    get_five_operation,
+    get_sort_operation,
+    mask_numb,
+    mask_number
+)
 
-
-operation = get_data_json("operations.json")
+path_file = "operations.json"
+operation = get_data_json(path_file)
 new_operation = get_new_operation(operation)
 sort_operation = get_sort_operation(new_operation)
 five_last_operation = get_five_operation(sort_operation)
 
 for i in five_last_operation:
-    print(get_format_date(i['date']), i['description'], '\n', mask_number(i.get('from', 'Новый счет')),
-          "->", mask_numb(i["to"]), "\n", i["operationAmount"]["amount"], '\n\n')
+    print((''), get_format_date(i['date']), i['description'], '\n', mask_number(i.get('from', 'Новый счет')),
+          "->", mask_numb(i["to"]), "\n", i["operationAmount"]["amount"],
+          i["operationAmount"]['currency']['name'], '\n\n')
